e7ccf925729927d47e912bc3463ba7080bed0907
TomyTM <franciscotomasapariciobustos@gmail.com>
2018-07-18 18:08:17 +0200

    cambios rutas y paneles

--- routes/skype.js
+++
@@ -1,5 +0,0 @@
-<span class="skype-button bubble "></span>
-<script src="https://swc.cdn.skype.com/sdk/v1/sdk.min.js"></script>
-
-<div class="skype-button bubble" data-bot-id="YOUR_BOT_ID"></div>
-<script src="https://swc.cdn.skype.com/sdk/v1/sdk.min.js"></script>
